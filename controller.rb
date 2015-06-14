e74d553b9ef81ce88b1674d19a74278a511145ac
orm <omer.metel@gmail.com>
2015-06-13 22:02:36 -0400

    Update content.

--- controller.rb
+++ controller.rb
@@ -1,11 +1,16 @@
-# ignore everything needed to build
+# Ignore everything needed to build
 ignore 'Gemfile.lock'
 ignore 'Gemfile'
 ignore 'Rakefile'
-# ignore all the git stuff
+
+# Ignore all the git stuff
 ignore '.gitignore'
 ignore '.ruby-version'
 ignore '.git'
-# we don't want to render the readme
+
 ignore 'README.md'
+ignore 'README (Verti).txt'
+ignore 'LICENSE.txt'
+
+ignore 'README.txt'
 
